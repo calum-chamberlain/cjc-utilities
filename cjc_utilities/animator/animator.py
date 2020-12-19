7228c7c0aaecbc2a3725241633b95695373c7022
calum-chamberlain <calum.chamberlain@vuw.ac.nz>
2020-12-19 15:44:31 +1300

    Add features to blank map

--- cjc_utilities/animator/animator.py
+++ cjc_utilities/animator/animator.py
@@ -69,8 +69,8 @@ def _blank_map(lons, lats, color, projection="global",
                water_fill_color='1.0', colormap=None, colorbar=None,
                title=None, colorbar_ticklabel_format=None,
                color_label="Depth (km)", logarithmic_color=False,
-               proj_kwargs=None,
-               figsize=(20, 20), pad=0.05, norm=None):
+               proj_kwargs=None, figsize=(20, 20), pad=0.05, norm=None,
+               fig=None, map_ax=None, cm_ax=None):
     """
     Plot a map for the region appropriate for the catalog, but do not plot the
     events themselves.
@@ -188,7 +188,8 @@ def _blank_map(lons, lats, color, projection="global",
     else:
         datetimeplot = False
 
-    fig = plt.figure(figsize=figsize)
+    if fig is None:
+        fig = plt.figure(figsize=figsize)
 
     # The colorbar should only be plotted if more then one event is
     # present.
@@ -277,16 +278,30 @@ def _blank_map(lons, lats, color, projection="global",
 
     # TODO: Add option to show cumulative plot
     if show_colorbar:
-        map_ax = fig.add_axes([ax_x0, 0.13, ax_width, 0.77], projection=proj)
-        cm_ax = fig.add_axes([ax_x0, 0.05, ax_width, 0.05])
+        if map_ax is None:
+            map_ax = fig.add_axes([ax_x0, 0.13, ax_width, 0.77], 
+                                  projection=proj)
+        elif not hasattr(map_ax, "projection") or map_ax.projection != proj:
+            geom = map_ax.get_geometry()
+            # Remove and rebuild
+            map_ax.remove()
+            map_ax = fig.add_subplot(*geom, projection=proj)
+        if cm_ax is None:
+            cm_ax = fig.add_axes([ax_x0, 0.05, ax_width, 0.05])
         plt.sca(map_ax)
     else:
         ax_y0, ax_height = 0.05, 0.85
         if projection == "local":
             ax_y0 += 0.05
             ax_height -= 0.05
-        map_ax = fig.add_axes([ax_x0, ax_y0, ax_width, ax_height],
-                              projection=proj)
+        if map_ax is None:
+            map_ax = fig.add_axes([ax_x0, ax_y0, ax_width, ax_height],
+                                  projection=proj)
+        elif not hasattr(map_ax, "projection") or map_ax.projection != proj:
+            geom = map_ax.get_geometry()
+            # Remove and rebuild
+            map_ax.remove()
+            map_ax = fig.add_subplot(*geom, projection=proj)
 
     if projection == 'local':
         x0, y0 = proj.transform_point(lon_0, lat_0, proj.as_geodetic())
