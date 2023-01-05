d0b6d21bf59b432ebef6fb766bf5e860c7ea3a85
Calum <calum.chamberlain@vuw.ac.nz>
2023-01-06 09:35:04 +1300

    Cope with silly formatting

--- cjc_utilities/get_geonet_basic_info.py
+++ cjc_utilities/get_geonet_basic_info.py
@@ -43,10 +43,10 @@ def get_geonet_events(startdate, enddate, bbox=(163.96, -49.18, 182.6, -32.3),
         reader = [line for line in reader]
         for line in reader:
             event_info.append(
-                {"latitude": float(line[' latitude']),
-                 "longitude": float(line['longitude']),
-                 "depth": float(line[' depth']),
-                 "magnitude": float(line[' magnitude']),
+                {"latitude": float(line.get(' latitude', line['latitude'])),  # Cope with dumb formatting
+                 "longitude": float(line.get(' longitude', line['longitude'])),
+                 "depth": float(line.get(' depth', line['depth'])),
+                 "magnitude": float(line.get(' magnitude', line['magnitude'])),
                  "origin-time": dt.strptime(line['origintime'],
                                             "%Y-%m-%dT%H:%M:%S.%fZ"),
                  "id": line['publicid']})
